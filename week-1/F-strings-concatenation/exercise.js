4c7070ddab6b6b3b7ea3e1a8ffe94d9441006866
davidlucky@gmail.com <davidlucky@gmail.com>
2019-08-03 08:37:18 -0500

    comparion

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -1,12 +1,7 @@
 // Start by creating a variable `message`
 
-console.log(message);
 
 var greetingStar = 'Hola, mi nombre es ';
-var name = 'David';
-
-var mensaje1 = 'greetingStar' + name;
-
-
-
+var name = 'David'
+var mensaje1 = greetingStar + name;
 console.log(mensaje1);

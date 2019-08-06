58b00b314813c785cd22f1757e7c022cebd6bbb0
davidlucky@gmail.com <davidlucky@gmail.com>
2019-08-06 12:17:06 -0500

    continuidad de ejercicios

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -3,3 +3,43 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function nombreGenericoMentor1() 
+{   
+    var mentorPrimero = mentor1.toUpperCase(); 
+    return `HELLO ${mentorPrimero}`;        
+}
+
+function nombreGenericoMentor2() 
+{  
+    var mentorDos = mentor2.toUpperCase(); 
+    return `HELLO ${mentorDos}`;         
+}
+
+function nombreGenericoMentor3() 
+{  
+    var mentorTres = mentor3.toUpperCase(); 
+    return `HELLO ${mentorTres}`;         
+}
+
+function nombreGenericoMentor4() 
+{  
+    var mentorCuatro = mentor3.toUpperCase(); 
+    return `HELLO ${mentorCuatro}`;         
+}
+
+function nombreGenericoMentor5() 
+{  
+    var mentorCinco = mentor5.toUpperCase(); 
+    return `HELLO ${mentorCinco}`;         
+}
+console.log(nombreGenericoMentor1(mentor1));
+console.log(nombreGenericoMentor2(mentor2));
+console.log(nombreGenericoMentor3(mentor3));
+console.log(nombreGenericoMentor4(mentor4));
+console.log(nombreGenericoMentor5(mentor5));
+
+
+
+
+

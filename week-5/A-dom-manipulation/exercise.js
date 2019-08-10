3dbfaafe1f7bed18e56ebc4c7170c1c85f2de3d9
davidlucky@gmail.com <davidlucky@gmail.com>
2019-08-10 15:01:41 -0500

    desarrolo week5

--- week-5/A-dom-manipulation/exercise.js
+++ week-5/A-dom-manipulation/exercise.js
@@ -4,17 +4,23 @@ Task 1
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-//git 
+var cambiarColorFondoBoton = document.querySelector('#bgrChangeBtn');
+cambiarColorFondoBoton.addEventListener("click", function() {
+    let color = "yellow"; 
+    document.body.style.backgroundColor = color;
+});
 
-
+console.log(cambiarColorFondoBoton);
 /*
 Task 2
 ======
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
-
-
+var botonAlert = document.querySelector('#alertBtn');
+botonAlert.addEventListener('click', function() {
+    alert ("Thanks for visiting Bikes for Refugees!")
+} )
 
 /*
 Task 3
@@ -22,8 +28,13 @@ Task 3
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
-
-
+let botondeLeerMas = document.querySelector('#addTextBtn'); //inner es para pintar en el html
+let cajadeTexto = document.querySelector('.jumbotron'); //appendchild es para crearlo pero no lo //muestra, queda en memoria.
+let creandoPdeParrafo = document.createElement('p'); 
+botondeLeerMas.addEventListener('click', function(){
+cajadeTexto.appendChild(creandoPdeParrafo);
+creandoPdeParrafo.innerHTML = 'Read more below.';
+});
 
 /*
 Task 4

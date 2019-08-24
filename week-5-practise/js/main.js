b718d40f82ba74b63a1ce08ae03f3b07e8c0f78a
davidlucky@gmail.com <davidlucky@gmail.com>
2019-08-24 11:08:31 -0500

    Ejercio del manejo del DOM

--- week-5-practise/js/main.js
+++ week-5-practise/js/main.js
@@ -1,14 +1,31 @@
-let myBlueBtn = document.querySelector("#blueBtn");
-let myOrange = document.querySelector("#orangeBtn");
+// Blue Colour
 
+let myBlueBtn = document.querySelector("#blueBtn");
 let myJumbotron = document.querySelector(".jumbotron");
 
-myBlueBtn.addEventListener("click", changeColor);
-function changeColor() {
-  myJumbotron.style.backgroundColor = "#6495ED";
+myBlueBtn.addEventListener("click", changeColorBlue);
+function changeColorBlue() {
+  myJumbotron.style.backgroundColor = "#588fbd";
 }
 
-myOrangeBtn.addEventListener("click", changeColor);
-function changeColor() {
-  myJumbotron.style.backgroundColor = "#f0ad4e";
+// Orange Colour
+
+let myOrangeBtn = document.querySelector("#orangeBtn");
+let colorOrange = document.querySelector(".jumbotron");
+
+myOrangeBtn.addEventListener("click", changeColorOrange);
+
+function changeColorOrange() {
+  colorOrange.style.backgroundColor = "#f0ad4e";
+}
+
+// Green Colour
+
+let myGreenBtn = document.querySelector("#greenBtn");
+let colorGreen = document.querySelector(".jumbotron");
+
+myGreenBtn.addEventListener("click", changeColorGreen);
+
+function changeColorGreen() {
+  colorGreen.style.backgroundColor = "#87ca8a";
 }
